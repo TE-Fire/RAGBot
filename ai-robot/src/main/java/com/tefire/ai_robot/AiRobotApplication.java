947668a4d48903a7d56391707c2b3d9d2d9e13d7
TE-Fire <3037749727@qq.com>
2026-07-08 21:44:21 +0800

    feat: 初始化智能问答agent Spring Boot项目
    创建基础项目结构，包含Spring Boot启动类、application配置文件和Maven构建配置，引入webmvc和Deepseek AI依赖

---
+++ ai-robot/src/main/java/com/tefire/ai_robot/AiRobotApplication.java
@@ -0,0 +1,13 @@
+package com.tefire.ai_robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRobotApplication.class, args);
+	}
+
+}
